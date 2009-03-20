a087423ae7de9e07109208acca0fb568d6a0165a
jay0lee <jay0lee@9cea05ea-14a3-11de-9308-ab3afd9c0aba>
2009-03-20 19:58:17 +0000

    start of work on allowing users to login as other users

--- gail/main.py
+++ gail/main.py
@@ -23,15 +23,13 @@ class ShowLogin(webapp.RequestHandler):
     domain = settings.GAPPS_DOMAIN
     if self.request.get('SAMLRequest') == '':
       self.redirect('https://mail.google.com/a/' + domain)
-      return
     SAMLRequest = self.request.get('SAMLRequest')
     age = utils.getSAMLRequestAge(SAMLRequest)
     if (age < 0) or (age > 590): # is our SAMLRequest old or invalid?
       self.redirect('https://mail.google.com/a/' + domain)
-      return
     template_values = {
     #we want to refresh 10 sec before SAMLRequest expires
-      'refresh': 590 - age,
+      'refresh': int(590 - age),
       'samlrequest': self.request.get('SAMLRequest'),
       'relaystate': self.request.get('RelayState'),
       'error': self.request.get('Error'),
@@ -44,12 +42,12 @@ class ShowLogin(webapp.RequestHandler):
 class DoLogin(webapp.RequestHandler):
   def post(self):
     templatepath = os.path.join(os.path.dirname(__file__), 'templates')
-    adminattempt = False
+    becomeattempt = False
     loginvalue = str(self.request.get('username'))
     if settings.ADMINS_BECOME_USER and loginvalue.find('+') != -1:
       username = loginvalue[0:(loginvalue.find('+'))]
       loginuser = loginvalue[(loginvalue.find('+') + 1):]
-      adminattempt = True
+      becomeattempt = True
     else:
       username = loginvalue
     password = self.request.get('password')
@@ -61,64 +59,70 @@ class DoLogin(webapp.RequestHandler):
     except gdata.service.BadAuthentication:
       self.redirect('/?SAMLRequest='+urllib.quote(self.request.get('SAMLRequest'))+'&RelayState='+urllib.quote(self.request.get('RelayState'))+'&Error=Unknown%20Username%20or%20Password')
     #Verify admin status by looking self up...
-    if adminattempt:
+    if becomeattempt and settings.ADMINS_BECOME_USER:
       try:
         LookupUser = apps.RetrieveUser(username)
       except gdata.apps.service.AppsForYourDomainException , e:
         self.redirect('/?SAMLRequest='+urllib.quote(self.request.get('SAMLRequest'))+'&RelayState='+urllib.quote(self.request.get('RelayState'))+'&Error=You%20Are%20Not%20An%20Admin.')
-      username = loginuser
-    if self.request.get('SAMLRequest') == '' or self.request.get('SAMLRequest') == None:
-      self.redirect('/?SAMLRequest='+urllib.quote(self.request.get('SAMLRequest'))+'&RelayState='+urllib.quote(self.request.get('RelayState')))
-    else:
-      ranchars = 'abcdefghijklmnop'
-      responseid = ''
-      assertid = ''
-      for i in range(1, 40):
-        responseid += ranchars[random.randint(0,15)]
-        assertid += ranchars[random.randint(0,15)]
-      request = self.request.get('SAMLRequest')
-      request = base64.b64decode(request)
-      request = zlib.decompress(request, -8)
-      xmldoc = minidom.parseString(request)
-      template_values = {
-        'assertid': assertid,
-        'responseid': responseid,
-        'username': username,
-        'domain': domain,
-        'issueinstant': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime()),
-        'authninstant': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime()),
-        'acsurl': xmldoc.firstChild.attributes['AssertionConsumerServiceURL'].value,
-        'providername': xmldoc.firstChild.attributes['ProviderName'].value,
-        'requestid': xmldoc.firstChild.attributes['ID'].value,
-        'notbefore': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(int(time.time()) - (5 * 60))),  # 5 minutes ago
-        'notafter': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(int(time.time()) + (10 * 60)))  # 10 minutes from now
-        }
-      responsepath = os.path.join(templatepath, 'response.xml')
-      #response = template.render(responsepath, template_values)
-      digestpath = os.path.join(templatepath, 'digest.xml')
-      digestPart = template.render(digestpath, template_values)
-      digestPart = digestPart[0:(len(digestPart) - 2)] # template adds \n\n at end of string, remove it
-      digestSha1 = hashlib.sha1(digestPart)
-      digest = base64.b64encode(digestSha1.digest())
-      template_values.update({'digest': digest})
-      sipath = os.path.join(templatepath, 'response-signature-signedinfo.xml')
-      signedInfo = template.render(sipath, template_values)
-      signedInfo = signedInfo[0:(len(signedInfo) - 1)] # get rid of last newline
-      signvalue = base64.b64encode(key.hashAndSign(gdata.tlslite.utils.compat.stringToBytes(signedInfo)))      
-      keyinfo = key.write()
-      modulus = keyinfo[keyinfo.find('<n>')+3:keyinfo.find('</n>')]
-      exponent = keyinfo[keyinfo.find('<e>')+3:keyinfo.find('</e>')]
-      template_values.update({'signvalue': signvalue, 'modulus': modulus, 'exponent': exponent})
-      responsepath = os.path.join(templatepath, 'response.xml')
-      signedresponse = template.render(responsepath, template_values) 
-      autopostpath = os.path.join(templatepath, 'autopost.html')
-      autopost_values = {
-        'acsurl': xmldoc.firstChild.attributes['AssertionConsumerServiceURL'].value,
-        'signedresponse': base64.b64encode(signedresponse),
-        'relaystate': self.request.get('RelayState')
-        }
-      self.response.out.write(template.render(autopostpath, autopost_values))
-      #self.response.out.write("<html><body>\n\nResponse:\n\n"+response+"\n\nDigest:\n\n"+digest+"\n\nDigestPart:\n\n"+digestPart+"\n\nSigned Response:\n\n"+signedresponse+"\n\n</body></html>")
+      if LookupUser.login.admin == 'true':
+        username = loginuser
+      else:
+        self.redirect('/?SAMLRequest='+urllib.quote(self.request.get('SAMLRequest'))+'&RelayState='+urllib.quote(self.request.get('RelayState'))+'&Error=You%20Are%20Not%20An%20Admin.')
+    SAMLRequest = self.request.get('SAMLRequest')
+    if self.request.get('SAMLRequest') == '':
+      self.redirect('https://mail.google.com/a/' + domain)
+    age = utils.getSAMLRequestAge(SAMLRequest)
+    if (age < 0) or (age > 590): # is our SAMLRequest old or invalid?
+      self.redirect('https://mail.google.com/a/' + domain)
+    ranchars = 'abcdefghijklmnop'
+    responseid = ''
+    assertid = ''
+    for i in range(1, 40):
+      responseid += ranchars[random.randint(0,15)]
+      assertid += ranchars[random.randint(0,15)]
+    request = self.request.get('SAMLRequest')
+    request = base64.b64decode(request)
+    request = zlib.decompress(request, -8)
+    xmldoc = minidom.parseString(request)
+    template_values = {
+      'assertid': assertid,
+      'responseid': responseid,
+      'username': username,
+      'domain': domain,
+      'issueinstant': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime()),
+      'authninstant': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime()),
+      'acsurl': xmldoc.firstChild.attributes['AssertionConsumerServiceURL'].value,
+      'providername': xmldoc.firstChild.attributes['ProviderName'].value,
+      'requestid': xmldoc.firstChild.attributes['ID'].value,
+      'notbefore': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(int(time.time()) - (5 * 60))),  # 5 minutes ago
+      'notafter': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(int(time.time()) + (10 * 60)))  # 10 minutes from now
+      }
+    responsepath = os.path.join(templatepath, 'response.xml')
+    #response = template.render(responsepath, template_values)
+    digestpath = os.path.join(templatepath, 'digest.xml')
+    digestPart = template.render(digestpath, template_values)
+    digestPart = digestPart[0:(len(digestPart) - 2)] # template adds \n\n at end of string, remove it
+    digestSha1 = hashlib.sha1(digestPart)
+    digest = base64.b64encode(digestSha1.digest())
+    template_values.update({'digest': digest})
+    sipath = os.path.join(templatepath, 'response-signature-signedinfo.xml')
+    signedInfo = template.render(sipath, template_values)
+    signedInfo = signedInfo[0:(len(signedInfo) - 1)] # get rid of last newline
+    signvalue = base64.b64encode(key.hashAndSign(gdata.tlslite.utils.compat.stringToBytes(signedInfo)))      
+    keyinfo = key.write()
+    modulus = keyinfo[keyinfo.find('<n>')+3:keyinfo.find('</n>')]
+    exponent = keyinfo[keyinfo.find('<e>')+3:keyinfo.find('</e>')]
+    template_values.update({'signvalue': signvalue, 'modulus': modulus, 'exponent': exponent})
+    responsepath = os.path.join(templatepath, 'response.xml')
+    signedresponse = template.render(responsepath, template_values) 
+    autopostpath = os.path.join(templatepath, 'autopost.html')
+    autopost_values = {
+      'acsurl': xmldoc.firstChild.attributes['AssertionConsumerServiceURL'].value,
+      'signedresponse': base64.b64encode(signedresponse),
+      'relaystate': self.request.get('RelayState')
+      }
+    self.response.out.write(template.render(autopostpath, autopost_values))
+    #self.response.out.write("<html><body>\n\nResponse:\n\n"+response+"\n\nDigest:\n\n"+digest+"\n\nDigestPart:\n\n"+digestPart+"\n\nSigned Response:\n\n"+signedresponse+"\n\n</body></html>")
 
 application = webapp.WSGIApplication(
                                      [('/dologin', DoLogin),
--- gail/settings.py
+++ gail/settings.py
@@ -20,3 +20,30 @@ GAPPS_DOMAIN = 'example.com'
 
 #ADMINS_BECOME_USER = False
 ADMINS_BECOME_USER = True
+
+# USERS_BECOME_USERS - If this settings is true and a valid Admin username and
+# password are set for ADMIN_USER and ADMIN_PASS, then you can assign regular
+# users the ability to login as other users.  Who is able to login as who is
+# determined by group membership.  So if you want the user MrSmith to be able
+# to log in as the user John, create a group in your Google Apps Dashboard
+# named John-Become and add MrSmith to the group.  If you want MrSmith to be
+# able to log in as John, Mary and George, add John, Mary and George ti the
+# group SixthGrade and add MrSmith to the group SixthGrade-Become.
+
+#USERS_BECOME_USERS = True
+USERS_BECOME_USERS = False
+
+# ADMIN_USER - The name of an account that is an admin of the Google Apps Domain
+# this account is used to lookup group membership for the USERS_BECOME_USERS
+# setting.  If USERS_BECOME_USERS is False, you do not need to set ADMIN_USER
+
+#ADMIN_USER = 'admin'
+ADMIN_USER = ''
+
+# ADMIN_PASS - The password of an account that is an admin of the Google Apps 
+# Domain this account is used to lookup group membership for the 
+# USERS_BECOME_USERS setting.  If USERS_BECOME_USERS is False, you do not need
+# to set ADMIN_PASS
+
+#ADMIN_PASS = 'p@ssw0rd'
+ADMIN_USER = ''
